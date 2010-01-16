95db37a8dcd6dcdb8a582f0f99d904dd2da5a496
andrea@VEGETA <andrea@VEGETA.(none)>
2010-01-16 16:25:18 +0100

    console app OK

--- lib/redspider.rb
+++ lib/redspider.rb
@@ -8,11 +8,12 @@ require File.dirname(__FILE__) + "/services/mechanize_service.rb"
 require File.dirname(__FILE__) + "/services/search_service.rb"
 require File.dirname(__FILE__) + "/services/evaluator_service.rb"
 
-query = Query.new(["Francesco", "Lissoni", "unibs"])
+query = Query.new(ARGV)
 searcher = SearchService.new
 matches = searcher.search(query)
 evaluator = EvaluatorService.new
 evaluator.evaluate_matches(matches, query)
-matches.sort { |a,b| a.score <=> b.score }.reverse.each do |m|
-  p "#{m.score} - #{m.email}"
+max_score = matches.max { |a,b| a.score <=> b.score }.score
+matches.sort { |a,b| a.score <=> b.score }.select { |m| m.score == max_score }.each do |m|
+  p "#{m.email}" if m.score == max_score
 end
